eccb8bb6f1347ca420a2130bd25baea600beee21
juanmera01 <Uo277406@uniovi.es>
2023-08-28 16:37:47 +0200

    b

--- src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
+++ src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
@@ -1,6 +1,5 @@
 package com.graalvmonlambda.product;
 
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -9,7 +8,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRequestHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
@@ -20,7 +18,7 @@ public class ProductRequestHandler implements RequestHandler<APIGatewayV2HTTPEve
 
         AmazonDynamoDB db = AmazonDynamoDBClientBuilder.standard().build();
         DynamoDBMapper mapper = new DynamoDBMapper(db);
-        
+
         List<Customer> customers;
         customers = mapper.scan(Customer.class, new DynamoDBScanExpression());
 
