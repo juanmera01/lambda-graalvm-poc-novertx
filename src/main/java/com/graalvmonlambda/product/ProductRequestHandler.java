cf145258695ac6f728b38988326d9ff8f3abe2b5
juanmera01 <Uo277406@uniovi.es>
2023-08-28 15:25:07 +0200

    change dynamodb dependency

--- src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
+++ src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
@@ -1,40 +1,26 @@
 package com.graalvmonlambda.product;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.enhanced.dynamodb.model.Page;
-import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRequestHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-    DynamoDbClient dynamodbClient = DynamoDbClient.create();
-    TableSchema<Customer> customerSchema = TableSchema.fromBean(Customer.class);
-    final DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
-            .dynamoDbClient(dynamodbClient).build();
-    DynamoDbTable<Customer> customerTable = enhancedClient.table(
-            "dxcassure-feature-127-java-lambda-research-service-customer_table",
-            customerSchema
-    );;
+    AmazonDynamoDB db = AmazonDynamoDBClientBuilder.defaultClient();
+    DynamoDBMapper mapper = new DynamoDBMapper(db);
 
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
 
-
-
-        List<Customer> customers = new ArrayList<>();
-        PageIterable<Customer> res = customerTable.scan();
-        for(Page<Customer> p : res){
-            customers.addAll(p.items());
-        }
+        List<Customer> customers;
+        customers = mapper.scan(Customer.class, new DynamoDBScanExpression());
 
         return APIGatewayV2HTTPResponse.builder()
                 .withBody(customers.toString())
