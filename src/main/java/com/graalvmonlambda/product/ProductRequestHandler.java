5d78191f9e99188adfbc1bab2dfda0d5d7db5ea1
juanmera01 <Uo277406@uniovi.es>
2023-08-29 10:28:24 +0200

    a

--- src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
+++ src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
@@ -5,6 +5,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.util.ArrayList;
@@ -12,17 +14,40 @@ import java.util.List;
 
 public class ProductRequestHandler implements RequestHandler<Request, Object> {
 
+    static final TableSchema<Customer> customerTableSchema =
+            TableSchema.builder(Customer.class)
+                    .newItemSupplier(Customer::new)
+                    .addAttribute(Long.class, a -> a.name("id")
+                            .getter(Customer::getId)
+                            .setter(Customer::setId)
+                            .tags(StaticAttributeTags.primaryPartitionKey()))
+                    .addAttribute(String.class, a -> a.name("email")
+                            .getter(Customer::getEmail)
+                            .setter(Customer::setEmail)
+                            .tags(StaticAttributeTags.primarySortKey()))
+                    .addAttribute(String.class, a -> a.name("name")
+                            .getter(Customer::getName)
+                            .setter(Customer::setName))
+                    .addAttribute(String.class, a -> a.name("firstname")
+                            .getter(Customer::getFirstname)
+                            .setter(Customer::setFirstname))
+                    .addAttribute(String.class, a -> a.name("city")
+                            .getter(Customer::getCity)
+                            .setter(Customer::setCity))
+                    .addAttribute(String.class, a -> a.name("street")
+                            .getter(Customer::getStreet)
+                            .setter(Customer::setStreet))
+                    .build();
 
     @Override
     public Object handleRequest(Request request, Context context) {
 
         DynamoDbClient dynamodbClient = DynamoDbClient.create();
-        TableSchema<Customer> customerSchema = TableSchema.fromBean(Customer.class);
         final DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
                 .dynamoDbClient(dynamodbClient).build();
         DynamoDbTable<Customer> customerTable = enhancedClient.table(
                 "dxcassure-feature-127-java-lambda-research-service-customer_table",
-                customerSchema
+                customerTableSchema
         );
 
         List<Customer> customers = new ArrayList<>();
